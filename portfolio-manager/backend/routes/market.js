9ffa64368e98937c9c7b3ab5d6ec80fb22790fec
ivy <l1693684248@163.com>
2025-07-29 01:40:34 +0000

    add performance

--- portfolio-manager/backend/routes/market.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMarketData, updateMarketData } = require('../controllers/market');
-
-router.get('/', getMarketData);
-router.post('/:symbol/update', updateMarketData);
-
-module.exports = router;
